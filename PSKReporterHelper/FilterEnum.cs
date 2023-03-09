40dcf649d2c54339e22100f009332f4591de7c40
grmurray <grmurray@qinetiq.com>
2023-03-09 18:05:20 +0000

    code laid out better

--- PSKReporterHelper/FilterEnum.cs
+++ PSKReporterHelper/FilterEnum.cs
@@ -3,14 +3,15 @@ using System;
 
 namespace PSKReporterHelper
 {
-    public enum FilterEnum 
+    public enum FilterEnum
     {
-       fifteen=15,
+        fifteen = 15,
         twenty = 20,
-        thirty =30,
-       hour=60,
-        twelveHour = (12*60),
-        lastday =(24*60)
+        thirty = 30,
+        hour = 60,
+        _6_hour = (6*60),
+        _12_Hour = (12 * 60),
+        lastday = (24 * 60)
     }
 
 }
